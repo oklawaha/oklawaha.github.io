27607a6aec55a8bbc8b83f4314404d7579089b6b
oklawaha <jdrost@hendersoncountync.org>
2020-10-01 15:05:24 -0400

    attempt to use GPX files directly... not successful

---
+++ leaflet/json/jsontemplate.js
@@ -0,0 +1,34 @@
+var jsongpx = {
+    "type": "FeatureCollection",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "EPSG:4326"
+        }
+    },
+
+    "features": [
+
+        {
+            "type": "Feature",
+            "id": 0,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-82.357154414389115, 35.460934471859076]
+            },
+            "properties": {}
+        },
+
+
+        {
+            "type": "Feature",
+            "id": 0,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-82.357154414389115, 35.460934471859076]
+            },
+            "properties": {}
+        },
+
+    ]
+};
